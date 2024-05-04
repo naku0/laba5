f41ab6e662f61a84f46125b0248f179619ae147b
Alex <409073@niuitmo.ru>
2024-05-05 00:46:41 +0300

    new jdoc

--- src/mainClasses/Main.java
+++ src/mainClasses/Main.java
@@ -7,14 +7,12 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Ваши данные сохраняются в: " + System.getenv("FILE_PATH"));
+        System.out.println("Ваши данные сохраняются в: " + System.getenv("FILE_PATH"));
         CommandManager commandManager = new CommandManager();
         CollectionManager collectionManager = new CollectionManager();
         Input input = new Input(commandManager, collectionManager);
         input.addCommands(commandManager);
         input.addData();
         input.listen();
-         */
-        Set<Integer> list = new <>();
     }
 }
